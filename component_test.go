0d476c30ae07447aede61a88243a1ec087f49fbe
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-06-10 22:19:33 -0300

    compare with unsafe implementation (parallel memory)

--- component_test.go
+++ component_test.go
@@ -1,27 +1,32 @@
-package ecs
+package ecs_test
 
-import "testing"
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/gabstv/ecs"
+)
 
 type TestMarco struct {
-	X float64
-	Y float64
-	Z float64
+	X float64 // 8 bytes
+	Y float64 // 8 bytes
+	Z float64 // 8 bytes
 }
 
 type TestPolo struct {
-	Scale float64
+	Scale float64 // 8 bytes
 }
 
 func BenchmarkViews100000(b *testing.B) {
 	b.StopTimer()
-	w := NewWorld()
-	c1, _ := w.NewComponent(NewComponentInput{
+	w := ecs.NewWorld()
+	c1, _ := w.NewComponent(ecs.NewComponentInput{
 		Name: "cmarco",
 	})
-	c2, _ := w.NewComponent(NewComponentInput{
+	c2, _ := w.NewComponent(ecs.NewComponentInput{
 		Name: "cpolo",
 	})
-	w.NewSystem("smarco", 100, func(ctx Context) {
+	w.NewSystem("smarco", 100, func(ctx ecs.Context) {
 		matches := ctx.System().View().Matches()
 		for _, v := range matches {
 			a := v.Components[c1].(*TestMarco)
@@ -39,7 +44,7 @@ func BenchmarkViews100000(b *testing.B) {
 		}
 		return c
 	}
-	w.NewSystem("smarcopolo", 99, func(ctx Context) {
+	w.NewSystem("smarcopolo", 99, func(ctx ecs.Context) {
 		matches := ctx.System().View().Matches()
 		for _, v := range matches {
 			a := v.Components[c1].(*TestMarco)
@@ -58,5 +63,86 @@ func BenchmarkViews100000(b *testing.B) {
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		w.Run(1 / 60)
+	}
+}
+
+const szmarco = int(unsafe.Sizeof(TestMarco{}))
+const szpolo = int(unsafe.Sizeof(TestPolo{}))
+
+func BenchmarkUnsafeParallelViews100000(b *testing.B) {
+	b.StopTimer()
+	//
+	fmax := func(a, b, c float64) float64 {
+		if a > b && a > c {
+			return a
+		}
+		if b > c {
+			return b
+		}
+		return c
+	}
+	//
+	marcomap := make(map[ecs.Entity]int)
+	polomap := make(map[ecs.Entity]int)
+	marcodata := make([]byte, szmarco*100000)
+	polodata := make([]byte, szpolo*(100000))
+	//
+	marcobytes := func(m TestMarco) []byte {
+		b := *(*[szmarco]byte)(unsafe.Pointer(&m))
+		return b[:]
+	}
+	polobytes := func(m TestPolo) []byte {
+		b := *(*[szpolo]byte)(unsafe.Pointer(&m))
+		return b[:]
+	}
+	//
+	moff := 0
+	poff := 0
+	firstmap := make([]ecs.Entity, 0, (100000))
+	combomap := make([]ecs.Entity, 0, (100000/7)+1)
+	for i := 0; i < 100000; i++ {
+		e := ecs.Entity(i + 1)
+		copy(marcodata[moff:moff+szmarco], marcobytes(TestMarco{}))
+		marcomap[e] = moff
+		moff += szmarco
+		firstmap = append(firstmap, e)
+		if i%7 == 0 {
+			copy(polodata[poff:poff+szpolo], polobytes(TestPolo{}))
+			polomap[e] = poff
+			poff += szpolo
+			combomap = append(combomap, e)
+		}
+	}
+	//
+	mbuffer := [szmarco]byte{}
+	marcob2s := func(v []byte, offset int) TestMarco {
+		copy(mbuffer[:], v[offset:offset+szmarco])
+		return *(*TestMarco)(unsafe.Pointer(&mbuffer))
+	}
+	//
+	pbuffer := [szpolo]byte{}
+	polob2s := func(v []byte, offset int) TestPolo {
+		copy(mbuffer[:], v[offset:offset+szmarco])
+		return *(*TestPolo)(unsafe.Pointer(&pbuffer))
+	}
+	//
+	b.StartTimer()
+	//
+	for bi := 0; bi < b.N; bi++ {
+		for i := 0; i < len(firstmap); i++ {
+			e := firstmap[i]
+			marco := marcob2s(marcodata, marcomap[e])
+			marco.X += .1
+			marco.Y += .2
+			marco.Z += .3
+			copy(marcodata[marcomap[e]:marcomap[e]+szmarco], marcobytes(marco))
+		}
+		for i := 0; i < len(combomap); i++ {
+			e := combomap[i]
+			marco := marcob2s(marcodata, marcomap[e])
+			polo := polob2s(polodata, polomap[e])
+			polo.Scale = fmax(marco.X, marco.Y, marco.Z)
+			copy(polodata[polomap[e]:polomap[e]+szpolo], polobytes(polo))
+		}
 	}
 }
